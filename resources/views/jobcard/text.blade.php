226b820ab6e108c748639dcadbdb9ec841f3bd57
mauchtec <trusttrinity8@gmail.com>
2023-04-27 08:07:13 +0200

    upload

--- resources/views/jobcard/text.blade.php
+++ resources/views/jobcard/text.blade.php
@@ -1,15 +1,8 @@
-@extends('layouts.app')
-@section('content')
-<meta charset="utf-8">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 
-<form action="{{route('done')}}" method="post" enctype="multipart/form-data">
+<form action="{{secure_url('done')}}" method="post" enctype="multipart/form-data">
   @csrf
 <input type="file" name="sql_file" id="sql_file">
 
 <button type="submit">Upload</button>
 </form>
-@endsection
