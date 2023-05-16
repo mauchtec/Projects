a8e90f743f45463dc4b64c28f84fa32d2bfeb260
mauchtec <trusttrinity8@gmail.com>
2023-05-16 23:55:07 +0200

    kkkpoiuy

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,32 +1,250 @@
 <!DOCTYPE HTML>
 <html>
 <style>
-.droptarget {
-  width: 100px; 
-  height: 30px;
-  padding: 5px;
+
+#div1, #div2,#div3 {
+        width: 30%;
+      height: auto;
+     
+      margin: 10px;
+      padding: 10px;
+      display: inline-block;
+      vertical-align: top;
+
+
+  float: left;
+  
   border: 1px solid black;
+}
+.search-box {
+    position: relative;        
+    float: right;
+}
+.search-box input {
+    height: 34px;
+    border-radius: 20px;
+    padding-left: 35px;
+    border-color: #ddd;
+    box-shadow: none;
+}
+.search-box input:focus {
+    border-color: #3FBAE4;
+}
+.search-box i {
+    color: #a0a5b1;
+    position: absolute;
+    font-size: 19px;
+    top: 8px;
+    left: 10px;
+}
+
+.column,.droptarget {
+      width: 20%;
+      height: auto;
+      border: 1px solid black;
+      margin: 10px;
+      padding: 10px;
+      display: inline-block;
+      vertical-align: top;
+    }
+
+    
+    #column1 {
+    
+      background-color: rgb(255, 4, 4);
+      
+    }
+    #column2 {
+     
+      background-color: rgb(14, 146, 14);
+     
+    }
+    #column3 {
+     
+      background-color: rgb(53, 50, 226);
+     
+    }
+
+    .item {
+      width: 90%;
+      height: 50px;
+      background-color: rgb(0, 0, 0);
+      color: white;
+      text-align: center;
+      line-height: 50px;
+      margin: 5px auto;
+      cursor: move;
+    }
+
+    .active {
+      border: dashed 2px red;
+    }
+    body {
+	color: #566787;
+	background: #f5f5f5;
+	font-family: 'Varela Round', sans-serif;
+	font-size: 13px;
+}
+.table-responsive {
+    margin: 30px 0;
+}
+.table-wrapper {
+	background: #fff;
+	padding: 20px 25px;
+	border-radius: 3px;
+	min-width: 1000px;
+	box-shadow: 0 1px 1px rgba(0,0,0,.05);
+}
+.table-title {        
+	padding-bottom: 15px;
+	background: #435d7d;
+	color: #fff;
+	padding: 16px 30px;
+	min-width: 100%;
+	margin: -20px -25px 10px;
+	border-radius: 3px 3px 0 0;
+}
+.table-title h2 {
+	margin: 5px 0 0;
+	font-size: 24px;
+}
+.table-title .btn-group {
+	float: right;
+}
+.table-title .btn {
+	color: #fff;
+	float: right;
+	font-size: 13px;
+	border: none;
+	min-width: 50px;
+	border-radius: 2px;
+	border: none;
+	outline: none !important;
+	margin-left: 10px;
+}
+.table-title .btn i {
+	float: left;
+	font-size: 21px;
+	margin-right: 5px;
+}
+.table-title .btn span {
+	float: left;
+	margin-top: 2px;
+}
+table.table tr th, table.table tr td {
+	border-color: #e9e9e9;
+	padding: 12px 15px;
+	vertical-align: middle;
+}
+table.table tr th:first-child {
+	width: 60px;
+}
+table.table tr th:last-child {
+	width: 100px;
+}
+table.table-striped tbody tr:nth-of-type(odd) {
+	background-color: #fcfcfc;
+}
+table.table-striped.table-hover tbody tr:hover {
+	background: #f5f5f5;
+}
+table.table th i {
+	font-size: 13px;
+	margin: 0 5px;
+	cursor: pointer;
+}	
+table.table td:last-child i {
+	opacity: 0.9;
+	font-size: 22px;
+	margin: 0 5px;
+}
+table.table td a {
+	font-weight: bold;
+	color: #566787;
+	display: inline-block;
+	text-decoration: none;
+	outline: none !important;
+}
+table.table td a:hover {
+	color: #2196F3;
+}
+table.table td a.edit {
+	color: #FFC107;
+}
+table.table td a.delete {
+	color: #F44336;
+}
+table.table td i {
+	font-size: 19px;
+}
+table.table .avatar {
+	border-radius: 50%;
+	vertical-align: middle;
+	margin-right: 10px;
+}
+
+/* Modal styles */
+.modal .modal-dialog {
+	max-width: 400px;
+}
+.modal .modal-header, .modal .modal-body, .modal .modal-footer {
+	padding: 20px 30px;
+}
+.modal .modal-content {
+	border-radius: 3px;
+	font-size: 14px;
+}
+.modal .modal-footer {
+	background: #ecf0f1;
+	border-radius: 0 0 3px 3px;
+}
+.modal .modal-title {
+	display: inline-block;
+}
+.modal .form-control {
+	border-radius: 2px;
+	box-shadow: none;
+	border-color: #dddddd;
+}
+.modal textarea.form-control {
+	resize: vertical;
+}
+.modal .btn {
+	border-radius: 2px;
+	min-width: 100px;
+}	
+.modal form label {
+	font-weight: normal;
 }
 </style>
 
 <body>
 <h1>HTML DOM Drag Events</h1>
-<div class="row">
-    <div class="col">
-<div class="droptarget">
-  <p draggable="true" id="dragtarget">Drag me!</p>
+
+
+<div class="droptarget" id="column1">
+  <p draggable="true" id="dragtarget1">Drag me!1</p>
+  <p draggable="true" id="dragtarget2">Drag me!2</p>
+  <p draggable="true" id="dragtarget3">Drag me!3</p>
 </div>
-    </div>
-    <div class="col">
-        <div class="droptarget"></div>
+
+
+
+<div class="droptarget" id="column2">
+    <p draggable="true" id="dragtarget4">Drag me!4</p>
+  <p draggable="true" id="dragtarget5">Drag me!5</p>
+  <p draggable="true" id="dragtarget6">Drag me!6</p>
+</div>
+<div class="droptarget" id="column3">
+  <p draggable="true" id="dragtarget7">Drag me!7</p>
+  <p draggable="true" id="dragtarget8">Drag me!8</p>
+  <p draggable="true" id="dragtarget9">Drag me!9</p>
+</div>
 
 <p id="demo"></p>
-    </div>
-</div>
 
 
 
-<p>Drag the text up and down between the two rectangles.</p>
 
 
 
