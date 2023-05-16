d3fb3bf5381b2e07bf98ab9594576d5780c077bc
mauchtec <trusttrinity8@gmail.com>
2023-05-16 23:17:48 +0200

    hh

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,54 +1,97 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-<head>
-	<title>Drag and Drop for Touchscreens</title>
-	<style>
-		.container {
-			display: flex;
-			flex-wrap: wrap;
-			width: 400px;
-			height: 400px;
-			border: 2px solid black;
-			padding: 10px;
-		}
-		
-		.box {
-			background-color: yellow;
-			width: 80px;
-			height: 80px;
-			margin: 10px;
-			padding: 10px;
-			border: 2px solid black;
-			cursor: move;
-		}
-	</style>
-</head>
+<style>
+.droptarget {
+  width: 100px; 
+  height: 30px;
+  padding: 5px;
+  border: 1px solid black;
+}
+</style>
+
 <body>
-	<div class="container">
-		<div class="box" draggable="true">Box 1</div>
-		<div class="box" draggable="true">Box 2</div>
-		<div class="box" draggable="true">Box 3</div>
-		<div class="box" draggable="true">Box 4</div>
-	</div>
+<h1>HTML DOM Drag Events</h1>
+<div class="row">
+    <div class="col">
+<div class="droptarget">
+  <p draggable="true" id="dragtarget">Drag me!</p>
+</div>
+    </div>
+    <div class="col">
+        <div class="droptarget"></div>
 
-	<script>
-		var dragItem = null;
+<p id="demo"></p>
+    </div>
+</div>
 
-		document.addEventListener("dragstart", function(e) {
-			dragItem = e.target;
-			e.dataTransfer.setData("text/html", dragItem.innerHTML);
-		});
 
-		document.addEventListener("dragover", function(e) {
-			e.preventDefault();
-		});
 
-		document.addEventListener("drop", function(e) {
-			if (e.target.classList.contains("box")) {
-				e.target.innerHTML = dragItem.innerHTML;
-				dragItem.innerHTML = e.dataTransfer.getData("text/html");
-			}
-		});
-	</script>
+<p>Drag the text up and down between the two rectangles.</p>
+
+
+
+<script>
+/* Events fired on the drag target */
+
+document.addEventListener("dragstart", function(event) {
+  // The dataTransfer.setData() method sets the data type and the value of the dragged data
+  event.dataTransfer.setData("Text", event.target.id);
+  
+  // Output some text when starting to drag the p element
+  document.getElementById("demo").innerHTML = "Started to drag the p element.";
+  
+  // Change the opacity of the draggable element
+  event.target.style.opacity = "0.4";
+});
+
+// While dragging the p element, change the color of the output text
+document.addEventListener("drag", function(event) {
+  document.getElementById("demo").style.color = "red";
+});
+
+// Output some text when finished dragging the p element and reset the opacity
+document.addEventListener("dragend", function(event) {
+  document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+  event.target.style.opacity = "1";
+});
+
+/* Events fired on the drop target */
+
+// When the draggable p element enters the droptarget, change the DIVS's border style
+document.addEventListener("dragenter", function(event) {
+  if ( event.target.className == "droptarget" ) {
+    event.target.style.border = "3px dotted red";
+  }
+});
+
+// By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
+document.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
+
+// When the draggable p element leaves the droptarget, reset the DIVS's border style
+document.addEventListener("dragleave", function(event) {
+  if ( event.target.className == "droptarget" ) {
+    event.target.style.border = "";
+  }
+});
+
+/* On drop - Prevent the browser default handling of the data (default is open as link on drop)
+   Reset the color of the output text and DIV's border color
+   Get the dragged data with the dataTransfer.getData() method
+   The dragged data is the id of the dragged element ("drag1")
+   Append the dragged element into the drop element
+*/
+document.addEventListener("drop", function(event) {
+  event.preventDefault();
+  if ( event.target.className == "droptarget" ) {
+    document.getElementById("demo").style.color = "";
+    event.target.style.border = "";
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+  }
+});
+</script>
+
 </body>
 </html>
