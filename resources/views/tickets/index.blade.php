a221c4e1724ba61313db19879c5496d9cb6084d9
mauchtec <trusttrinity8@gmail.com>
2023-05-16 22:53:52 +0200

    test

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,37 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <style>
-    #dragTarget {
-      width: 200px;
-      height: 200px;
-      border: 1px solid black;
-      background-color: lightgray;
-      text-align: center;
-      padding: 10px;
-    }
-  </style>
-  <script>
-    function dragStart(event) {
-      event.dataTransfer.setData("text/plain", event.target.id);
-    }
-
-    function allowDrop(event) {
-      event.preventDefault();
-    }
-
-    function drop(event) {
-      event.preventDefault();
-      var data = event.dataTransfer.getData("text/plain");
-      event.target.appendChild(document.getElementById(data));
-    }
-  </script>
+	<title>Drag and Drop for Touchscreens</title>
+	<style>
+		.container {
+			display: flex;
+			flex-wrap: wrap;
+			width: 400px;
+			height: 400px;
+			border: 2px solid black;
+			padding: 10px;
+		}
+		
+		.box {
+			background-color: yellow;
+			width: 80px;
+			height: 80px;
+			margin: 10px;
+			padding: 10px;
+			border: 2px solid black;
+			cursor: move;
+		}
+	</style>
 </head>
 <body>
-  <div id="dragTarget" ondrop="drop(event)" ondragover="allowDrop(event)">
-    <div draggable="true" ondragstart="dragStart(event)" id="dragElement">
-      Drag me!
-    </div>
-  </div>
+	<div class="container">
+		<div class="box" draggable="true">Box 1</div>
+		<div class="box" draggable="true">Box 2</div>
+		<div class="box" draggable="true">Box 3</div>
+		<div class="box" draggable="true">Box 4</div>
+	</div>
+
+	<script>
+		var dragItem = null;
+
+		document.addEventListener("dragstart", function(e) {
+			dragItem = e.target;
+			e.dataTransfer.setData("text/html", dragItem.innerHTML);
+		});
+
+		document.addEventListener("dragover", function(e) {
+			e.preventDefault();
+		});
+
+		document.addEventListener("drop", function(e) {
+			if (e.target.classList.contains("box")) {
+				e.target.innerHTML = dragItem.innerHTML;
+				dragItem.innerHTML = e.dataTransfer.getData("text/html");
+			}
+		});
+	</script>
 </body>
 </html>
